78dcd5bac4123e7d35aa48fbe91a16a0d7f0c8a3
Grigory.N <grigoriynbr@gmail.com>
2026-03-22 12:21:23 +0300

    add zsh install

---
+++ src/install.sh
@@ -0,0 +1,5 @@
+# install.sh in your dotfiles repo
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
