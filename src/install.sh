068771083218cdacd0cd917e0aca1b45d1fe9949
Grigory.N <grigoriynbr@gmail.com>
2026-03-22 12:48:45 +0300

    update zsh install

--- src/install.sh
+++ src/install.sh
@@ -1,5 +1,24 @@
-# install.sh in your dotfiles repo
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#!/bin/sh
 
+# Install Oh My Zsh (unattended so script continues)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Install Starship
+if [ "$(uname)" = "Darwin" ]; then
+    brew install starship
+elif [ -f /etc/os-release ]; then
+    . /etc/os-release
+    case "$ID" in
+        fedora)
+            dnf copr enable atim/starship
+            dnf install -y starship
+            ;;
+    esac
+fi
+
+# Add Starship to .zshrc if not already there
+grep -q "starship init zsh" ~/.zshrc || echo 'eval "$(starship init zsh)"' >> ~/.zshrc
